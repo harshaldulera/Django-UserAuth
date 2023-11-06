3fb6e17490198a6f757e880ac2e300827360e98c
Harshal Dulera <harshaldulera82@gmail.com>
2023-11-06 17:58:49 +0530

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        if(localStorage.getItem('access_token') === null){
+            window.location.href = '/signin'
+        } else {
+            (async () => {
+                try {
+                    const {data} = await axios.get('http://localhost:8000/home/', { headers: { 'Content-Type': 'application/json' } });
+
+                    setMessage(data.message);
+                } catch (e) {
+                    console.log('not authenticated');
+                }
+            })()};
+    }, [])
+
+
     return (
         <div className="home-container">
             <h1>Welcome to the Home Page</h1>
+            <h3>Hi {message}</h3>
             <div className="button-cont">
                 <Link to="/signin">
                     <button className="Sign-In">Sign In</button>
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function SignUp () {
+const SignUp = () => {
+    const [username, setUsername] = useState("");   
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Welcome To SignUp Page.</h1>
